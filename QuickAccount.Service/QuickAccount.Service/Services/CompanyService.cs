cf810487e99b276f8fd8167fdb7f2fe2169110a5
Jigar Tailor <jigartailor1984@live.com>
2022-07-17 22:46:15 +0530

    * Added new folder for Service contract. * Moved `company.proto` to newly created folder for contract.

---
+++ QuickAccount.Service/QuickAccount.Service/Services/CompanyService.cs
@@ -0,0 +1,16 @@
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickAccount.Service.Services
+{
+    public class CompanyService //: Service.CompanyService.CompanyServiceBase
+    {
+        //public override Task<RegisterCompanyResponse> RegisterNewCompany(RegisterCompanyRequest request, ServerCallContext context)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
